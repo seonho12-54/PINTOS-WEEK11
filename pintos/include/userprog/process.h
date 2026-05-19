e40e983b3537be0b28c78e20ae7bf977c2a4713f
chaekang <herry612@naver.com>
2026-05-19 19:53:54 +0900

    fix: lazy_load_segment 함수 static으로 변경

--- pintos/include/userprog/process.h
+++ pintos/include/userprog/process.h
@@ -12,7 +12,6 @@ int process_exec (void *f_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (struct thread *next);
-bool lazy_load_segment (struct page *page, void *aux);
 
 /* 지연 로딩 시 페이지 초기화에 필요한 파일 정보 묶음. */
 struct lazy_load_args {
